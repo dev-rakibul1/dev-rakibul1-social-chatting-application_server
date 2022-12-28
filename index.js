0b2bd7f2970a9df36bdc0d16328dc7e5b2dc631d
dev-rakibul1 <fl.rakibul@gmail.com>
2022-12-28 15:35:55 +0600

    social application server is ready for code

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 8000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// Middle ware
+app.use(cors());
+app.use(express.json());
+
+// mongodb collection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lqqbhox.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
+
+// =====================all db collection======================
+const postCollection = client.db("end_games_social_app").collection("posts");
+
+app.get("/posts", async (req, res) => {
+  try {
+    const query = {};
+    const result = await postCollection.find(query).toArray();
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome from server");
+});
+
+app.listen(port, () => {
+  console.log(`Server url: http://localhost:${port}/`);
+});
