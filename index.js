cbf62f7b19a7fd5b6ee90863776c6663c5831d50
dev-rakibul1 <fl.rakibul@gmail.com>
2022-12-29 15:56:24 +0600

    top api added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 8000;
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // Middle ware
 app.use(cors());
@@ -28,6 +28,7 @@ run().catch((error) => console.log(error));
 
 // =====================all db collection======================
 const postCollection = client.db("end_games_social_app").collection("posts");
+const AboutCollection = client.db("end_games_social_app").collection("about");
 
 app.get("/posts", async (req, res) => {
   try {
@@ -42,8 +43,49 @@ app.get("/posts", async (req, res) => {
   }
 });
 
+app.get("/top-posts", async (req, res) => {
+  try {
+    const query = {};
+    const result = await postCollection
+      .find(query)
+      .sort({ like: -1 })
+      .limit(3)
+      .toArray();
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
+});
+
+app.get("/about", async (req, res) => {
+  try {
+    const query = {};
+    const result = await AboutCollection.find(query).toArray();
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Welcome from server");
+});
+
+app.put("/about/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const filter = { _id: ObjectId(id) };
+  console.log("filter", filter);
+  const options = { upsert: true };
+  console.log("name", req.body.website);
+
+  const updateDoc = {
+    $set: req.body,
+  };
+
+  const result = await AboutCollection.updateMany(filter, updateDoc, options);
+  res.send(result);
+  console.log(result);
 });
 
 app.listen(port, () => {
